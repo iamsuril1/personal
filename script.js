1b4c529972eff66102b7c96b6ac886e16408a09a
Suril Pokharel <surilpokharel3@gmail.com>
2024-10-09 18:44:14 +0545

    Added css

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Typed.js for the dynamic text effect
-    var typed = new Typed('#element', {
-        strings: ['Web Developer', 'Web Designer', 'Content Writer.'],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true,
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  // Typed.js for the dynamic text effect
+  var typed = new Typed("#element", {
+    strings: ["Web Developer", "Web Designer", "Content Writer."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  });
 
-    // Animate skill bars
-    document.querySelector('.html').style.setProperty('--level', '90%');
-    document.querySelector('.css').style.setProperty('--level', '70%');
-    document.querySelector('.js').style.setProperty('--level', '60%');
-    document.querySelector('.react').style.setProperty('--level', '30%');
+  // Animate skill bars
+  document.querySelector(".html").style.setProperty("--level", "90%");
+  document.querySelector(".css").style.setProperty("--level", "70%");
+  document.querySelector(".js").style.setProperty("--level", "60%");
+  document.querySelector(".react").style.setProperty("--level", "30%");
 });
